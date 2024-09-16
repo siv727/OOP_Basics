aa98b7b3e91db0ddb34c92180a7609cbf1db2e31
siv-a-siv <143003088+siv727@users.noreply.github.com>
2024-09-16 23:06:44 +0800

    Added length and substr method

--- libraryRecreate/Main.java
+++ libraryRecreate/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Manipulate m = new Manipulate();
         String str = "Hello World!";
+        String str2 = "ginger";
         m.setStr(str);
         m.print();
+        System.out.println(m.len());
+        System.out.println(m.substr(str2, m.len()));
     }
 }
--- libraryRecreate/string/Manipulate.java
+++ libraryRecreate/string/Manipulate.java
@@ -12,6 +12,36 @@ public class Manipulate {
     }
 
     public void print() {
-        System.out.printf(this.str);
+        System.out.println(this.str);
+    }
+
+    public int len() {
+        int count = 0;
+        char newStr[] = this.str.toCharArray();
+        for (char c : newStr)
+            count++;
+        return count;
+    }
+
+    public String substr(String findStr, int size) {
+        char src[] = this.str.toCharArray();
+        char sub[] = findStr.toCharArray();
+        String foundSub = "";
+        int index = 0;
+        for (char e : src) {
+            index++;
+            if (e == sub[0]) {
+                break;
+            }
+        }
+        if (index != size) {
+            index -= 1;
+            for (int i = index; i < size; i++) {
+                foundSub = foundSub.concat(Character.toString(src[i]));
+            }
+            return foundSub;
+        } else {
+            return null;
+        }
     }
 }
